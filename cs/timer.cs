f929da49ddff1e01e9390ca9bf628108c975c3d7
just <562028810@qq.com>
2020-11-26 16:49:47 +0000

    rename

---
+++ cs/timer.cs
@@ -0,0 +1,27 @@
+using System;
+using System.IO;
+using System.Threading;
+using System.Timers;
+namespace tst {
+	public class pro {
+		int a = 666;
+		public static void Main () {
+			Console.WriteLine ("hello,mono");
+			Console.Write (35 + "\n");
+
+			System.Timers.Timer mtimer1;
+			mtimer1 = new System.Timers.Timer (100);
+			mtimer1.Elapsed += new ElapsedEventHandler (go);
+			mtimer1.AutoReset = true;
+			mtimer1.Enabled = true;
+			Thread.Sleep (3000)
+		}
+
+		public static void go (object o, ElapsedEventArgs e) {
+			int a = 666 * 666;
+			Console.ForegroundColor = ConsoleColor.Red;
+			Console.WriteLine ("gogogo");
+			Console.WriteLine (a);
+		}
+	}
+}
