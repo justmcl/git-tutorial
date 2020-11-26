acc1345520d82f1bb5eb303120a2d05187b13636
just <562028810@qq.com>
2020-11-26 04:50:20 +0000

    nithing

---
+++ cs/hello.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Timers
+using System.IO;
+
+namespace tst{
+	public class pro
+	{
+		public static void Main()
+		{
+			Console.WriteLine("hello,mono");
+			Console.Write(35);
+			private System.Timers.Timer mtimer0;
+			mtimer0 =new System.Timers.Timer(100);
+			mtimer0.Elapsed+=new ElapsedEventHandler(dosg)
+			mtimer0.AutoReset
+		}
+
+		private void go()
+		{
+			
+		}
+	}
+}
