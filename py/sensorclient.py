02f3b10f0e53043f1c861900a239d3f9914e968c
just <562028810@qq.com>
2020-11-27 04:06:33 +0000

    add all

---
+++ py/sensorclient.py
@@ -0,0 +1,21 @@
+import socket               # 导入 socket 模块
+import json
+ 
+dataSocket = socket.socket()         # 创建 socket 对象
+host = socket.gethostname() # 获取本地主机名
+port = 10001                # 设置端口号
+ 
+dataSocket.connect(("192.168.1.101", port))
+while True:
+    toSend=input('??').encode()
+    dataSocket.send(toSend)
+    received = dataSocket.recv(512000)
+    revstr = received.decode()
+    #try:
+        #status=json.loads(revstr)
+        #for i in range(0,6):
+            #print (status["torqsensor"][1][2][i])
+    #except:
+        #print("bad")
+    print(revstr)
+s.close()
