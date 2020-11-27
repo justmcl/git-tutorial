5441ef91f60ef66e4418cdaa426d29dc511407cb
just <chenglong.miao@jaka.com>
2020-11-27 13:03:54 +0800

    nothing

--- py/sensorclient.py
+++ py/sensorclient.py
@@ -4,18 +4,33 @@ import json
 dataSocket = socket.socket()         # 创建 socket 对象
 host = socket.gethostname() # 获取本地主机名
 port = 10001                # 设置端口号
- 
+js0="{"cmdName":"set_compliant_type","sensor_compensation":1,"compliance_type":1}"
+js1="{"cmdName":"get_compliant_type"}"
+jssend=""
 dataSocket.connect(("192.168.1.101", port))
+
 while True:
     toSend=input('??').encode()
-    dataSocket.send(toSend)
+    if toSend.rstrip()=="s0" :
+        dict=json.loads(js0)
+        dict0["compliance_type"]=0
+        jssend=dict.dumps(dict0)
+    elif toSend.rstrip()=="s10" :
+        dict=json.loads(js0)
+        dict0["compliance_type"]=1
+        jssend=dict.dumps(dict0)
+    elif toSend.rstrip()=="s20" :
+        dict=json.loads(js0)
+        dict0["compliance_type"]=2
+        jssend=dict.dupms(jso)
+    elif toSend.rstrip()=="g" :
+        dict=json.loads(js0)
+        dict0["compliance_type"]=2
+        jssend=dict.dupms(jso)
+
+    dataSocket.send(jssend)
     received = dataSocket.recv(512000)
     revstr = received.decode()
-    #try:
-        #status=json.loads(revstr)
-        #for i in range(0,6):
-            #print (status["torqsensor"][1][2][i])
-    #except:
-        #print("bad")
     print(revstr)
-s.close()
+    
+dataSocket.close()
