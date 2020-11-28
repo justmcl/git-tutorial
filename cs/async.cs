342fe20b6b0f721e0d0b145822ca3a85012056d0
just <562028810@qq.com>
2020-11-28 15:32:22 +0000

    great

--- cs/async.cs
+++ cs/async.cs
@@ -12,21 +12,24 @@ namespace AsyncTest
             Thread trd1 = new Thread(WriteY);
             trd1.Name = "W_Y_T";
             trd1.Start();
-            for (int i = 0; i < 1000; i++)
-            {
-                Console.ForegroundColor = ConsoleColor.Green;
-                //Console.Write(Thread.CurrentThread);
-                Console.Write("##xxx##");
-                //if (i == 10){trd1.Join();}
-            }
+            Thread.Sleep(1000);
+            // for (int i = 0; i < 1000; i++)
+            // {
+            //     //Console.ForegroundColor = ConsoleColor.Green;
+            //     //Console.Write(Thread.CurrentThread);
+            //     Console.Write("##xxx##");
+            //     //if (i == 10){trd1.Join();}
+            // }
         }
         static void WriteY()
         {
-            for (int i = 0; i < 1000; i++)
+            for (double i = 0; i < 1000000; i=i+0.1)
             {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write("##yyy##");
-                Thread.Sleep(0);
+                //Console.ForegroundColor = ConsoleColor.Red;
+                //Console.Write("##yyy##");
+                double  j=i*i*i;
+                Console.WriteLine("{0},{1}",i,j);
+                //Thread.Sleep(0);
             }
         }
     }
--- cs/timer.cs
+++ cs/timer.cs
@@ -7,8 +7,10 @@ namespace tst {
 		public int a = 0;
 		public static void Main () {
 			Console.WriteLine ("hello,mono");
-			dog kit = new dog ();
-			Thread.Sleep (10000);
+			
+			//dog kit = new dog ();
+			computer tab=new computer();
+			Thread.Sleep (1000);
 		}
 	}
 	public class dog {
@@ -38,6 +40,27 @@ namespace tst {
 				//}
 				Thread.Sleep (100);
 				Console.WriteLine ("({0},{1})",mnum,i);
+			}
+		}
+	}
+	public class computer {
+		public int age = 0;
+		public int number=0;
+		public computer () {
+			System.Timers.Timer mtimer1;
+			mtimer1 = new System.Timers.Timer (1);
+			mtimer1.Elapsed += new ElapsedEventHandler (go);
+			mtimer1.AutoReset = false;
+			mtimer1.Enabled = true;
+		}
+		public void go (object o, ElapsedEventArgs e) {
+			for (double i = 0; i < 1000000; i=i+0.1)
+            {
+                //Console.ForegroundColor = ConsoleColor.Red;
+                //Console.Write("##yyy##");
+                double  j=i*i*i;
+                Console.WriteLine("{0},{1}",i,j);
+                //Thread.Sleep(0);
 			}
 		}
 	}
