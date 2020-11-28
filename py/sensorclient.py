e2e19c235fee68f922002862de87d7caf42b74e0
just <562028810@qq.com>
2020-11-28 03:10:36 +0000

    add all

--- py/sensorclient.py
+++ py/sensorclient.py
@@ -4,31 +4,29 @@ import json
 dataSocket = socket.socket()         # 创建 socket 对象
 host = socket.gethostname() # 获取本地主机名
 port = 10001                # 设置端口号
-js0="{"cmdName":"set_compliant_type","sensor_compensation":1,"compliance_type":1}"
-js1="{"cmdName":"get_compliant_type"}"
+js0='{"cmdName":"set_compliant_type","sensor_compensation":1,"compliance_type":1}'
+js1='{"cmdName":"get_compliant_type"}'
 jssend=""
 dataSocket.connect(("192.168.1.101", port))
 
 while True:
-    toSend=input('??').encode()
+    toSend=input('??')
     if toSend.rstrip()=="s0" :
-        dict=json.loads(js0)
+        dict0=json.loads(js0)
         dict0["compliance_type"]=0
-        jssend=dict.dumps(dict0)
+        jssend=json.dumps(dict0)
     elif toSend.rstrip()=="s10" :
-        dict=json.loads(js0)
+        dict0=json.loads(js0)
         dict0["compliance_type"]=1
-        jssend=dict.dumps(dict0)
+        jssend=json.dumps(dict0)
     elif toSend.rstrip()=="s20" :
-        dict=json.loads(js0)
+        dict0=json.loads(js0)
         dict0["compliance_type"]=2
-        jssend=dict.dupms(jso)
+        jssend=json.dumps(js0)
     elif toSend.rstrip()=="g" :
-        dict=json.loads(js0)
-        dict0["compliance_type"]=2
-        jssend=dict.dupms(jso)
+        jssend=js1
 
-    dataSocket.send(jssend)
+    dataSocket.send(jssend.encode())
     received = dataSocket.recv(512000)
     revstr = received.decode()
     print(revstr)
